59da0b2fc010741bb2ff73f54d7a4caddf103ed6
AOZMH <zhangminhao@pku.edu.cn>
2020-09-15 22:34:27 +0800

    修改接口

--- RelationExtraction/model_based_re/re_api.py
+++ RelationExtraction/model_based_re/re_api.py
@@ -42,17 +42,15 @@ class kbqa_client():
         self.conn.close()
 
 
-def re_api(json_string):
+def re_api(dat):
     """
-    输入为string形式，可以通过json.dumps(json_obj)生成；之所以用string交互是为了跨语言/平台兼容，输入后通过json.loads转换、解析json参数
-    参数格式为[text, entity1, entity2, cand_rel]，分别为需要抽取信息的文本、文本中识别出的两个实体、这两个实体之间的候选关系
-    E.g. json_string = "[\"报道还提到，4月29日，两架B-1B轰炸机曾从美国本土远程奔赴中国南海参加演习。\", \"B-1B轰炸机\", \"美国\", [\"使用国家\", \"所属国家\"]]"
-
-    输出为[entity1, entity2, top_1_relation]，通过json.dumps以string格式输出
+    关系识别模型接口
     """
-    dat = json.loads(json_string)
-    ret = []
-    for text, entity1, entity2, cand_rel in dat:
+    text = dat['text']
+    for ix,itm in enumerate(dat['pairlist']):
+        entity1 = itm['entity1']
+        entity2 = itm['entity2']
+        cand_rel = itm['relations']
         assert(len(cand_rel)>0), "候选关系集为空集!"
         host = '115.27.161.60'
         port = 9303
@@ -60,13 +58,14 @@ def re_api(json_string):
         res = client.extract_relation(text, entity1, entity2, 1, cand_rel)
         client.stop_client()
         top1_rel = res['Result'][0][0]
-        ret.append([entity1, entity2, top1_rel])
+        dat['pairlist'][ix]['relation'] = top1_rel
+        del dat['pairlist'][ix]['relations']
     
-    return json.dumps(ret)
+    return dat
 
 
 if __name__ == "__main__":
-    json_string = "[[\"报道还提到，4月29日，两架B-1B轰炸机曾从美国本土远程奔赴中国南海参加演习。\", \"B-1B轰炸机\", \"美国\", [\"使用国家\", \"所属国家\"]]]"
-    output_json_string = re_api(json_string)
+    json_obj_input = {'text': '包头稀土高新区经信委主任陈福才表示，近期已有10户企业与银行对接，拟贷款9500万元。', 'pairlist': [{'entity1': '陈福才', 'entity2': '包头稀土高新区经信委主任', 'relations': ['职务']}, {'entity1': '陈福才', 'entity2': '包头稀土高新区经信委', 'relations': ['毕业院校', '任职']}, {'entity1': '包头稀土高新区经信委', 'entity2': '陈福才', 'relations': ['负责人', '创始人', '成员']}]}
+    output_json_obj = re_api(json_obj_input)
     #print(output_json_string)
-    print(json.loads(output_json_string))
+    print(output_json_obj)
